6756eae1704af231befde971af16264f7a5f69b9
Odenstig <erik.odenstig@live.se>
2022-10-11 23:42:48 +0200

    Add project files.

---
+++ Datakommunikation-Upgift-1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using Uppgift1.Client;
+
+//Client
+
+Console.OutputEncoding = Encoding.UTF8;
+
+//Basic object to be sent between client and server
+Person person = new();
+
+//Creates new TcpClient
+using TcpClient client = new();
+
+//Connects to our Server
+await client.ConnectAsync("localhost", 5006);
+
+//Creates new stream to allow reading/writing between client and server
+using var stream = client.GetStream();
+var reader = new StreamReader(stream, new UTF8Encoding(false));
+var writer = new StreamWriter(stream, new UTF8Encoding(false));
+
+//Some values for our object
+Console.Write("Name: ");
+person.Name = Console.ReadLine();
+Console.Write("Email: ");
+person.Email = Console.ReadLine();
+Console.Write("Address: ");
+person.Address = Console.ReadLine();
+
+//Serializes our object into Json 
+var json = JsonSerializer.Serialize(person);
+
+//Writes our serialized data to Streambuffer
+await writer.WriteAsync(json);
+//Clears Streambuffer and writes to server
+await writer.FlushAsync();
+
+while (true)
+{
+    //Creates and allocates bytes
+    var buffer = new byte[1024];
+
+    //Reads and stores received bytes in memory
+    int received = stream.Read(buffer);
+
+    //Converts stored bytes to string
+    string data = Encoding.UTF8.GetString(buffer.AsSpan(0, received));
+
+    //Deserializes data into person object
+    var jsonDeserialized = JsonSerializer.Deserialize<Person>(data);
+
+    //Serializes person object again readying it to be sent to stream
+    var jsonSerialized = JsonSerializer.Serialize(jsonDeserialized);
+
+    //Writes out data echoed from server
+    Console.Write("\r\n");
+    Console.WriteLine("Server echoed: {0}", jsonSerialized);
+}
---
+++ Datakommunikation-Uppgift-1-Server/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using Uppgift1.Client;
+
+//Server
+
+Console.OutputEncoding = Encoding.UTF8;
+
+//Creates new endpoint on localhost with port 5006
+var endpoint = new IPEndPoint(IPAddress.Loopback, 5006);
+
+//Creates new socket with the endpoint we created above, Stream SocketType to establish connection with client, and required Tcp Protocol
+using var socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+//Binds socket to endpoint
+socket.Bind(endpoint);
+
+//Places socket in listening state, listens to client requests
+socket.Listen();
+Console.WriteLine("listening on port {0}", endpoint.Port);
+
+//Awaits client connection
+var handler = await socket.AcceptAsync(CancellationToken.None);
+
+Console.Write("\r\n");
+Console.WriteLine("Connection Secured");
+
+while (true)
+{
+    //Creates and allocates bytes to buffer
+    var buffer = new byte[2048];
+
+    //Checks how many bytes have been received, fills buffer
+    var receivedByteLength = await handler.ReceiveAsync(buffer, SocketFlags.None);
+
+    //Converts received data on buffer to an array
+    var data = buffer.Take(receivedByteLength).ToArray();
+
+    //Converts data from bytes to string 
+    string response = Encoding.UTF8.GetString(data);
+
+    //Deserializes data into person object
+    var jsonDeserialized = JsonSerializer.Deserialize<Person>(data);
+
+    //Serializes our person object, readying it for server response
+    var jsonSerialized = JsonSerializer.Serialize(jsonDeserialized);
+
+    //Converts serialized data back into bytes for server response
+    buffer = Encoding.UTF8.GetBytes(jsonSerialized);
+
+    Console.Write("\r\n");
+    Console.WriteLine("Server recieved: {0}", response);
+
+    Console.Write("\r\n");
+    Console.WriteLine("Deserialized Name: {0}", jsonDeserialized.Name);
+    Console.WriteLine("Deserialized Email: {0}", jsonDeserialized.Email);
+    Console.WriteLine("Deserialized Address: {0}", jsonDeserialized.Address);
+
+    //Sends Server response back to client
+    await handler.SendAsync(buffer, SocketFlags.None);
+}
